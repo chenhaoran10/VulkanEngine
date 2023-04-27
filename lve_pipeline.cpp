8f244d93e0bff243dc27bc01f7d709042a67066b
xiaoxiong <906467339@qq.com>
2023-04-28 00:09:42 +0800

    feature:build up vkDevice.

--- lve_pipeline.cpp
+++ lve_pipeline.cpp
@@ -8,10 +8,6 @@
 
 namespace lve {
 
-    LvePipeline::LvePipeline(const std::string &vertFilepath, const std::string &fragFilepath) {
-        createGraphicsPipeline(vertFilepath, fragFilepath);
-    }
-
     std::vector<char> LvePipeline::readFile(const std::string &filePath) {
         std::ifstream file{filePath, std::ios::ate | std::ios::binary};
 
@@ -30,13 +26,37 @@ namespace lve {
         return buffer;
     }
 
+    LvePipeline::LvePipeline(LveDevice &device, const std::string &vertFilepath, const std::string &fragFilepath,
+                             const PipelineConfigInfo &configInfo) : lveDevice(device) {
+        createGraphicsPipeline(vertFilepath, fragFilepath, configInfo);
+
+    }
+
     void LvePipeline::createGraphicsPipeline(
-            const std::string &vertFilepath, const std::string &fragFilepath) {
+            const std::string &vertFilepath, const std::string &fragFilepath, const PipelineConfigInfo &configInfo) {
         auto vertCode = readFile(vertFilepath);
         auto fragCode = readFile(fragFilepath);
 
         std::cout << "Vertex Shader Code Size: " << vertCode.size() << '\n';
         std::cout << "Fragment Shader Code Size: " << fragCode.size() << '\n';
+    }
+
+    PipelineConfigInfo LvePipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height) {
+        PipelineConfigInfo configInfo{};
+        return configInfo;
+    }
+
+    void LvePipeline::createShaderModule(const std::vector<char> &code, VkShaderModule *shaderModule) {
+        VkShaderModuleCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+        createInfo.codeSize = code.size();
+        createInfo.pCode = reinterpret_cast<const uint32_t *>(code.data());
+
+        if (vkCreateShaderModule(lveDevice.device(), &createInfo, nullptr, shaderModule)
+            != VK_SUCCESS) {
+            throw std::runtime_error("failed to create shader module");
+        }
+
     }
 
 }  // namespace lve
--- lve_window.cpp
+++ lve_window.cpp
@@ -1,5 +1,6 @@
 #include "lve_window.hpp"
 
+#include <stdexcept>
 namespace lve {
 
     LveWindow::LveWindow(int w, int h, std::string name) : width{w}, height{h}, windowName{name} {
@@ -17,5 +18,12 @@ namespace lve {
 
         window = glfwCreateWindow(width, height, windowName.c_str(), nullptr, nullptr);
         glfwSetWindowUserPointer(window, this);
+    }
+
+    void LveWindow::createWindowSurface(VkInstance instance, VkSurfaceKHR *surface) {
+        if (glfwCreateWindowSurface(instance,window, nullptr,surface)!=VK_SUCCESS)
+        {
+            throw std::runtime_error("failed to create window surface");
+        }
     }
 }  // namespace lve
