4afcd7f333cc5545aab32916c9bd59b35e6e8a96
xiaoxiong <906467339@qq.com>
2023-04-27 22:11:12 +0800

    feature:rendering pipeline.

--- lve_app.hpp
+++ lve_app.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
 #include "lve_window.hpp"
+#include "lve_pipeline.hpp"
 
 namespace lve {
 
     class LveApp {
     public:
         LveApp() = default;
+
         LveApp(const LveApp &) = delete;
+
         LveApp &operator=(const LveApp &) = delete;
 
         void run();
@@ -17,5 +20,7 @@ namespace lve {
 
     private:
         LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+        LvePipeline lvePipeline{"C://Users//90646//Desktop//Vulkan/VulkanEngine/shaders/simple_shader.vert.spv",
+                                "C://Users//90646//Desktop//Vulkan/VulkanEngine/shaders/simple_shader.frag.spv"};
     };
 }  // namespace lve
